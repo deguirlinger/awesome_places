360a12a078fe45b0674c31b50a15d8b6244860a7
Doug <deguirlinger@gmail.com>
2019-07-27 16:33:02 -0700

    styling

--- App.js
+++ App.js
@@ -1,74 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
+  TextInput
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  state = {
+    placeName: ''
+  }
 
-const App = () => {
-  return (
-    <Fragment>
-      <Text>Something Changed!</Text>
-    </Fragment>
-  );
-};
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{width: 300}}
+          placeholder="Awesome Place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  container: {
+    flex: 1,
+    padding: 50,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }
 });
-
-export default App;
